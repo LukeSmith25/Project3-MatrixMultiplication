99296e38574564ff4101d3daf16b8b1f3f65c36b
Luke Smith <jlukesmith0221@gmail.com>
2023-03-03 17:21:48 -0600

    Initial Commit

---
+++ ThreadedMatrixMult.h
@@ -0,0 +1,62 @@
+/*
+* Author:                 Luke Smith
+* Assignment Title:       Matrix Multiplication
+* Assignment Description: This program computes the product of two matrices using four methods: Brute Force, Divide
+*                         and Conquer, Strassen's Algorithm, and Threaded Strassen's Algorithm
+* Due Date:               2/19/2023
+* Date Created:           3/3/2023
+* Date Last Modified:     3/3/2023 - Initial creation of files
+ */
+
+#ifndef PROJECT3_MATRIXMULTIPLICATION_THREADEDMATRIXMULT_H
+#define PROJECT3_MATRIXMULTIPLICATION_THREADEDMATRIXMULT_H
+
+#include <iostream>
+using namespace std;
+
+struct SquareMatrix{
+    int dim;
+    int** data;    // points to a [dim x dim] square matrix
+    SquareMatrix(int size) {
+        this->dim = size;
+        this->data = nullptr;
+    }
+};
+
+void * BruteForceSquareMatrixMultiplication(void *);
+
+static void displayMatrix(SquareMatrix* matrix) {
+    for (int i = 0; i < matrix->dim; i++) {
+        // For Size of Col B
+        for (int j = 0; j < matrix->dim; j++) {
+
+        }
+    }
+}
+
+//************************************************************
+// description: read data from specified file                *
+// return: string                                            *
+// pre: string is valid                                      *
+// post: returns string of data                              *
+//                                                           *
+//************************************************************
+SquareMatrix* BruteForce(const SquareMatrix& A, const SquareMatrix& B) {
+    SquareMatrix* result = new SquareMatrix(A.dim);
+    result->dim = A.dim;
+    // For Size of Row A
+    for (int i = 0; i < A.dim; i++) {
+        // For Size of Col B
+        for (int j = 0; j < B.dim; j++) {
+            // For Size of Col A (To Get Actual Values From A)
+            for (int k = 0; k < A.dim; k++) {
+                result->data[i][j] += A.data[i][k] * B.data[k][j];
+                cout << result->data[i][j] << " = " << A.data[i][k] << B.data[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+#endif //PROJECT3_MATRIXMULTIPLICATION_THREADEDMATRIXMULT_H
---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+* Author:                 Luke Smith
+* Assignment Title:       Matrix Multiplication
+* Assignment Description: This program computes the product of two matrices using four methods: Brute Force, Divide
+*                         and Conquer, Strassen's Algorithm, and Threaded Strassen's Algorithm
+* Due Date:               2/19/2023
+* Date Created:           3/3/2023
+* Date Last Modified:     3/3/2023 - Initial creation of files
+ */
+
+#include <iostream>
+#include "ThreadedMatrixMult.h"
+using namespace std;
+
+int main() {
+    int matrixA[3][3] = {10, 10, 20, 4, 5, 6, 2, 3, 5};
+    int matrixB[3][3] = {3, 2, 4, 3, 3, 9, 4, 4, 2};
+
+    auto* A = new SquareMatrix(3);
+    auto* B = new SquareMatrix(3);
+    A->data = (int**)(matrixA);
+    B->data = (int**)(matrixB);
+
+
+    return 0;
+}
